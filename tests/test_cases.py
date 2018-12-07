05e3d62e537e17b40260f945c599482ba50a02cc
Calin Cristescu <calin.cristescu@jobleads.de>
2018-12-07 17:51:03 +0100

    test

--- tests/test_cases.py
+++ tests/test_cases.py
@@ -7,7 +7,7 @@ import scrapy
 
 
 class BaseScrapySeleniumTestCase(TestCase):
-    """Base test case for the ``scrapy-selenium`` package"""
+    """Base test case for the ``scrapy-selenium-python`` package"""
 
     class SimpleSpider(scrapy.Spider):
         name = 'simple_spider'
